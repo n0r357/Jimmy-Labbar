a8fd7c586f0e2e722238085c62705187f25b48ee
Jimmy Waern <jimmy.waern@hotmail.com>
2016-10-12 13:35:39 +0200

    Workshop: Klasser och objekt

--- Labb1/Labb1/Dog.cs
+++ Labb1/Labb1/Dog.cs
@@ -8,20 +8,82 @@ namespace Labb1
 {
     class Dog
     {
+        public static bool isProgramOver;
         public string Name { get; set; }
         public int Age { get; set; }
         public string Breed { get; set; }
+        private List<string> dogs = new List<string>();
 
-
-        //public Dog(string name, int age, string breed)
-        //{
-        //    Name = name;
-        //    Age = age;
-        //    Breed = breed;
-        //}
-        public void Introduction(string name, int age, string breed)
+        public List<string> Dogs
         {
-            Console.WriteLine("The dog is named {0}, its {1} years old and its breed is {2}", Name, Age, Breed);
+            get { return dogs; }
+            set { dogs = value; }
         }
-    }
+
+        public string Introduction()
+        {
+            return String.Format(("The dog is named {0}, its {1} years old and its breed is a {2}."), Name, Age, Breed);
+        }
+        public void ShowList()
+        {
+            int index;
+            foreach (var dog in Dogs)
+            {
+                index = Dogs.IndexOf(dog) + 1;
+                Console.WriteLine(index + " - " + dog);
+            }
+        }
+        public void AddDog()
+        {
+            Console.WriteLine("Name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Breed: ");
+            Breed = Console.ReadLine();
+            string input = Name + "\t" + Age + "\t" + Breed;
+            Dogs.Add(input);
+        }
+        public void RemoveDog()
+        {
+            int input;
+            Console.Write("Select dog to remove: ");
+            input = int.Parse(Console.ReadLine());
+            Dogs.RemoveAt(input - 1);
+            Console.WriteLine("Dog removed, press key to continue.");
+            Console.ReadKey();
+        }
+        public static void Menu()
+        {
+            Dog newDog = new Dog();
+            string choice;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Add dog");
+                Console.WriteLine("2 - Remove dog");
+                Console.WriteLine("3 - Show list");
+                Console.WriteLine("4 - Exit");
+                Console.Write("Your choice: ");
+                switch (choice = Console.ReadLine())
+                {
+                    case "1":
+                        newDog.AddDog();
+                        break;
+                    case "2":
+                        newDog.RemoveDog();
+                    break;
+                    case "3":
+                        newDog.ShowList();
+                        break;
+                    case "4":
+                        isProgramOver = true;
+                        break;
+                    default:
+                        break;
+                }
+            } while (true); 
+       } 
+}
 }
--- Labb1/Labb1/Runtime.cs
+++ Labb1/Labb1/Runtime.cs
@@ -10,26 +10,54 @@ namespace Labb1
     {
         public void Start()
         {
-            var dog1 = new Dog
-            {
-                Name = "Bus",
-                Age = 12,
-                Breed = "Papillion"
-            };
-            var dog2 = new Dog
-            {
-                Name = "Ralf",
-                Age = 12,
-                Breed = "Papillion"
-            };
-            var dog3 = new Dog
-            {
-                Name = "Dino",
-                Age = 14,
-                Breed = "Samojed"
-            };
+            #region Del 1:
 
-            Console.WriteLine();
+            //var dog1 = new Dog
+            //{ 
+            //    Name = "Bus",
+            //    Age = 12,
+            //    Breed = "Papillion"
+            //};
+            //var dog2 = new Dog
+            //{
+            //    Name = "Ralf",
+            //    Age = 12,
+            //    Breed = "Papillion"
+            //};
+            //var dog3 = new Dog
+            //{
+            //    Name = "Dino",
+            //    Age = 14,
+            //    Breed = "Samojed"
+            //};
+
+            //Console.WriteLine(dog1.Introduction());
+            //Console.WriteLine(dog2.Introduction());
+            //Console.WriteLine(dog3.Introduction());
+
+            #endregion
+
+            #region Del 2:
+
+            //List<Dog> dogs = new List<Dog>()
+            //{
+            //    new Dog { Name = "Bus", Age = 12, Breed = "Papillion" },
+            //    new Dog { Name = "Ralf", Age = 12, Breed = "Papillion" },
+            //    new Dog { Name = "Dino", Age = 14, Breed = "Samojed" }
+            //};
+            //foreach (var dog in dogs)
+            //{
+            //    Console.WriteLine(dog.Introduction());
+            //}
+
+            #endregion
+
+            Dog.isProgramOver = false;
+
+            do
+            {
+                Dog.Menu();
+            } while (Dog.isProgramOver);
         }
     }
 }
