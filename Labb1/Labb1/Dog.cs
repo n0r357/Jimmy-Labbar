985a421e72c5c430a68085092180e5769c2f5fad
Jimmy Waern <Jimmy Waern>
2016-10-12 11:38:37 +0200

    Workshop: Klasser och objekt

---
+++ Labb1/Labb1/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+
+        //public Dog(string name, int age, string breed)
+        //{
+        //    Name = name;
+        //    Age = age;
+        //    Breed = breed;
+        //}
+        public void Introduction(string name, int age, string breed)
+        {
+            Console.WriteLine("The dog is named {0}, its {1} years old and its breed is {2}", Name, Age, Breed);
+        }
+    }
+}
---
+++ Labb1/Labb1/Runtime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1
+{
+    class Runtime
+    {
+        public void Start()
+        {
+            var dog1 = new Dog
+            {
+                Name = "Bus",
+                Age = 12,
+                Breed = "Papillion"
+            };
+            var dog2 = new Dog
+            {
+                Name = "Ralf",
+                Age = 12,
+                Breed = "Papillion"
+            };
+            var dog3 = new Dog
+            {
+                Name = "Dino",
+                Age = 14,
+                Breed = "Samojed"
+            };
+
+            Console.WriteLine();
+        }
+    }
+}
